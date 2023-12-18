8bbc3bd084da4adedfad1c50ad992f718185ae98
Eliana Torres <elianatorres3114@gmail.com>
2023-12-17 23:38:37 -0500

    things

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -5,8 +5,7 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display the home page.
 ================================================== */
 import React from "react";
-import "";
-import { Link } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const HomePageView = (props) => {
   // Render Home page view
